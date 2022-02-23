9d78e769c591d27ed117fb3d292848724f99fae6
Keks <keks@htmlacademy.ru>
2022-02-23 17:58:59 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,26 +1,25 @@
 // Вывод рандомного числа, взято с ресурса:
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
 function getRandomInt(min, max) {
-  max = Math.floor(max + 1)
-  min = Math.ceil(min)
+  max = Math.floor(max + 1);
+  min = Math.ceil(min);
 
   if (max < min) {
-    max = Math.floor(max - 1)
-    min = Math.ceil(min + 1)
+    max = Math.floor(max - 1);
+    min = Math.ceil(min + 1);
   }
 
-  return Math.floor(Math.random() * (max - min)) + min
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-console.log(getRandomInt(10, 10))
+getRandomInt(10, 100);
 
-// Посчитать длину текста
-const letterMax = 140
-var letter = document.querySelector(".test-js").textContent.length
-
-if (letter < letterMax) {
-  console.log("Длина вашего комментария " + letter + "/" + letterMax)
-
-} else {
-  console.log("Вы привысили максимальную длину комментария " + letter + "/" + letterMax)
+// Проверка количества символов в комментарии
+function checkStringLength (inputString, letterMax) {
+  if (inputString.length <= letterMax) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+checkStringLength('string to test', 140);
