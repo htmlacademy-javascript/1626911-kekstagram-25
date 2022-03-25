5fe0d2ac283f056f343bc330de9843c30d10d559
Keks <keks@htmlacademy.ru>
2022-03-25 09:54:31 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/formWorkValidation.js
+++ 7/js/formWorkValidation.js
@@ -4,8 +4,7 @@
 const uploadForm = document.querySelector('.img-upload__form');
 // В форме поиск окна ввода с хэш-тегом
 const hashTags = uploadForm.querySelector('.text__hashtags');
-// Допустимые символы в форме
-
+// Место вывода ошибки о валидации хэштега
 const hashTagsValidText = document.querySelector('.text__error-hashtag');
 
 const pristine = new Pristine(uploadForm, {
@@ -31,26 +30,45 @@ function validateHashTags(value) {
   const arrHashTags = value.split(' ');
   const uniqArrHashTags = new Set(arrHashTags);
 
+
   if (arrHashTags.length > 5) {
-    hashTagsValidText.textContent = 'Хэш-тегов не может быть более 5 :(';
+    hashTagsValidText.textContent = 'Хэш-тегов не может быть более 5';
     return false;
   }
   else {
     hashTagsValidText.textContent = '';
     if (uniqArrHashTags.size < arrHashTags.length) {
-      hashTagsValidText.textContent = 'Не должно быть повторяющихся Хэш-тегов :(';
+      hashTagsValidText.textContent = 'Не должно быть повторяющихся Хэштегов';
       return false;
     }
     else {
       hashTagsValidText.textContent = '';
-      //const re = new RegExp(/^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/);
-      // for (let i = 0; i > 5; i = i + 1) {
-      //   re.test(arrHashTags[i]);
-      // }
     }
-    console.log('123');
-    return true;
   }
+
+  const re = new RegExp('^#[A-Za-zА-Яа-яЁё0-9]{1,20}$');
+  for (let i = 0; i < arrHashTags.length; i++) {
+
+    if (hashTags.value.length > 0) {
+      const curentHashtag = arrHashTags[i];
+      const hashCheck = re.test(curentHashtag);
+      if (hashCheck === false) {
+        hashTagsValidText.textContent = 'Хэш-тег имеет ошибку или длину более 20 символов';
+        if (curentHashtag === '' || curentHashtag === '#') {
+          hashTagsValidText.textContent = 'Введите #ХэшТег';
+        }
+        return false;
+      }
+      else {
+        hashTagsValidText.textContent = '';
+      }
+    } else {
+      hashTagsValidText.textContent = '';
+      return true;
+    }
+
+  }
+  return true;
 }
 
 pristine.addValidator(hashTags, validateHashTags, 'Ошибка валидации');
