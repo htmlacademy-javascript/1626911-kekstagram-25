ae8004472652f34dab3893382fb30a22fc2a1f50
Kirill <kirill-nef@mail.ru>
2022-03-29 16:09:36 +0500

    ДЗ module8task2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 // 1 Функция создания рандомных пользователей пользователей (Имя/коммент/аватар и тд)
 import './createArrayUsers.js';
-// 2 Функция создания миниатюр на странице
+// 2 Модуль создания массива с фотографиями
+import './createArrayPicture.js';
+// 3 Функция создания миниатюр на странице
 import './renderingThumbnails.js';
-// 3 Функция проверки длины комментария
-import {checkStringLength} from './checkStringLength.js';
-// 4 Функция получения рандомного числа
-import {getRandomPositiveInteger} from './getRandomPositiveInteger.js';
-// 5 Функция открытия большой картинки
+// 4 Функция открытия большой картинки
 import './openBigPicture.js';
-
-
-// 3 Вызов Функции проверки длины комментария
-checkStringLength('Test', 18);
-
-// 4 Вызов Функции получения рандомного числа
-getRandomPositiveInteger(0, 500);
+// 5 Модуль работы с формой
+import './formWork.js';
+// 6 Модуль подкачки комментов, если их больше 5ти
+import './downloadComments.js';
