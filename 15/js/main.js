3f4b608cf3f2cba3e9165912896d5b581b9bc4dc
Keks <keks@htmlacademy.ru>
2022-04-14 12:13:57 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/main.js
@@ -0,0 +1,31 @@
+
+//Модуль получения данных от сервера
+import { getData } from './api.js';
+// Модуль отправки формы с фотографией
+import './formWork.js';
+// Модуль изменения размера картинки в форме
+import './formWorkScale.js';
+// Модуль вызова сообщения
+import { showAlert } from './util.js';
+// Модель тестовый
+import { getSorting } from './sorting.js';
+
+// Массив данных, подгружается из getData с  сервера
+let photoDataArray;
+
+// Массив для импорта
+const getOriginalDataArray = () => photoDataArray;
+
+getData().then((data) => {
+  photoDataArray = data;
+})
+  .then(() => {
+    if (photoDataArray === false) {
+      showAlert('Не удалось загрузить данные с сервера, попробуйте обновить зайти позднее!', 16000);
+    }
+    else {
+      getSorting();
+    }
+  });
+
+export { photoDataArray, getOriginalDataArray };
