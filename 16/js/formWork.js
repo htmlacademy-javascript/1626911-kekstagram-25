17e3b6fdb05f20234a9c3fafcca5777c2e93edec
Keks <keks@htmlacademy.ru>
2022-04-15 09:58:21 +0300

    :heavy_check_mark: Сборка #16

---
+++ 16/js/formWork.js
@@ -0,0 +1,57 @@
+// Модуль для работы с формой
+
+// Импорт функции закрытия окна по escape
+import {isEscapeKey} from './util.js';
+// Закрытие слайдера
+import  {closeSlider} from './formWorkSlider.js';
+// Поля вывода ошибок по инпутам
+import  {hashTagsValidText, descriptionValidText, fieldComment, fieldHashtags} from './formWorkValidation.js';
+import {loadComments} from './downloadComments.js';
+
+// Поиск окна выгрузки
+const imgUpload = document.querySelector('.img-upload__overlay');
+// Поиск кнопки закрытия окна выгрузки
+const cancellButton = imgUpload.querySelector('.cancel');
+// Инпут с данными
+const imgUploadInput = document.querySelector('.img-upload__input');
+
+const uploadImg = document.querySelector('.img-upload__preview img');
+
+// Функция для закрытия окна с большой фотографией по нажатию Escape
+const onPopupEscapeKeydown = (evt) => {
+  if (isEscapeKey(evt) && evt.target !== fieldHashtags && evt.target !== fieldComment) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+// Удаление обработчика функции закрытия по Escape
+function closePopup () {
+  imgUpload.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscapeKeydown);
+  imgUploadInput.value = '';
+  hashTagsValidText.textContent = '';
+  descriptionValidText.textContent = '';
+  uploadImg.style.transform = 'none';
+  const bigPictureSocial = document.querySelector('.big-picture__social');
+  const commentsLoader = bigPictureSocial.querySelector('.comments-loader');
+  commentsLoader.removeEventListener('click', loadComments);
+  closeSlider();
+}
+
+// Функция закрытия окна выгрузки по крестику
+cancellButton.addEventListener('click', () => {
+  closePopup();
+});
+
+// Открытие окна выгрузки при изменении инпута
+imgUploadInput.onchange = function() {
+  imgUpload.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  // Подключение функции закрытия большого окна по нажатию Escape
+  document.addEventListener('keydown', onPopupEscapeKeydown);
+};
+
+export {closePopup};
---
+++ 16/js/formWorkSlider.js
@@ -0,0 +1,69 @@
+import { effectConfig } from './formWorkData.js';
+
+// Бокс под слайдер
+const boxSlider = document.querySelector('.img-upload__effect-level');
+// Поле слайдера
+const sliderElement = document.querySelector('.effect-level__slider');
+// Поле значения слайдера
+const valueElement = document.querySelector('.effect-level__value');
+// Поиск списка радио кнопок
+const effectsList = document.querySelector('.effects__list');
+// Поиск фотографии
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
+// Выбранное value фильтра из radio кнопки
+let chekedValue;
+// CSS название эффекта
+let effectName;
+// Метод счисления
+let method = '';
+
+// Создание слайдера
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
+
+// Слежение за изменением слайдера
+sliderElement.noUiSlider.on('update', () => {
+  // Меняет значение в input
+  valueElement.value = sliderElement.noUiSlider.get();
+  controlSlider();
+});
+
+// Получение от радио кнопки выбранный фильтр
+effectsList.addEventListener('change', (evt) => {
+  chekedValue = evt.target.closest('input[type="radio"]').value;
+  sliderSetting();
+});
+
+// Функция применяющая эффект на фотографию. Вызывается при работе слайдера.
+function controlSlider () {
+  imgUploadPreview.style.filter = `${ effectName }(${ valueElement.value + method})`;
+  return '';
+}
+
+// Функция подгрузки настроек для фильтров. Вызывается при изменении активной радио кнопки.
+function sliderSetting () {
+  // Если нет эффекта
+  if (chekedValue === 'none') {
+    closeSlider();
+  } else {
+    boxSlider.classList.remove('hidden');
+    const effect = effectConfig[chekedValue];
+    effectName = effect.effectProperty;
+    method = effect.unit;
+    sliderElement.noUiSlider.updateOptions(effect.sliderConfig);
+  }
+}
+
+function closeSlider () {
+  imgUploadPreview.style.filter = 'none';
+  boxSlider.classList.add('hidden');
+}
+
+export {closeSlider};
